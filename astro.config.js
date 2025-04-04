3752b554308bd0307c7815edbaf950dcc115757c
LineXic <linexic@yeah.net>
2025-04-04 11:09:52 +0800

    删除了上一篇按钮，优化了cc协议，添加了友链

--- astro.config.js
+++ astro.config.js
@@ -4,12 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-
-
+import { defineConfig } from 'astro/config';
 
 
 // https://astro.build/config
 export default defineConfig({
+
+
   // 请修改为你自己的线上地址，谢谢茄子
   site: 'https://www.linexic.top/',
 
@@ -27,3 +28,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   }
 });
+
+
+
+
