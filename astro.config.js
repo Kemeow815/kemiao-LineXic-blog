80b5d66dcb7137834b7adefe63e6f43f20c706ce
LineXic <linexic@yeah.net>
2025-04-11 15:27:48 +0800

    修复归档

--- astro.config.js
+++ astro.config.js
@@ -3,19 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
-import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-
 
 // https://astro.build/config
 export default defineConfig({
-
-
   // 请修改为你自己的线上地址，谢谢茄子
   site: 'https://www.linexic.top/',
 
-  base: process.env.NODE_ENV === "production" ? "/" : "",
-  integrations: [mdx(), sitemap(), svelte(), icon()],
+  // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
+  // 在根路径下（例如 `https://example.com/`）则填写 `/`
+  base: process.env.NODE_ENV === "production" ? "/koi/" : "",
+
+  integrations: [mdx(), sitemap(), svelte()],
 
   markdown: {
     remarkRehype: {
@@ -28,7 +26,3 @@ export default defineConfig({
     plugins: [tailwindcss()]
   }
 });
-
-
-
-
