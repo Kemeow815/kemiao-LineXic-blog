f9037e4f4c90889f6b822129b6dfabe7baf1c73c
Kemeow815 <kemiao@kmblog.icu>
2025-05-28 22:19:30 +0800

    改了下基础url，然后删了原有的文章，增加了我自己的文章

--- astro.config.js
+++ astro.config.js
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   // 请修改为你自己的线上地址，谢谢茄子
-  site: 'https://www.linexic.top',
+  site: 'https://blog-v2.kemeow.top',
   // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
   // 在根路径下（例如 `https://example.com/`）则填写 `/`
   base: process.env.NODE_ENV === "production" ? "/" : "",
